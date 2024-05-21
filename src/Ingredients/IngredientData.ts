3b1f4252489418792d4a1d4dfee4a83c94285c25
keonwoo98 <keonwoo98@naver.com>
2024-05-21 12:53:05 +0200

    [feat] add ingredients

--- src/Ingredients/IngredientData.ts
+++ src/Ingredients/IngredientData.ts
@@ -71,3 +71,80 @@ export const cucumber: IngredientType = {
   type: "vegetables",
   unit: "unit",
 };
+
+export const lobster: IngredientType = {
+  name: "Homard",
+  type: "fruit de mer",
+  unit: "entier",
+  prefix: "un"
+};
+
+export const rice: IngredientType = {
+  name: "Riz Arborio",
+  type: "riz",
+  unit: "g",
+  prefix: "de"
+};
+
+export const whiteWine: IngredientType = {
+  name: "Vin blanc",
+  type: "vin",
+  unit: "ml",
+  prefix: "de",
+};
+
+export const chickenBroth: IngredientType = {
+  name: "Bouillon de poulet",
+  type: "bouillon",
+  unit: "ml",
+  prefix: "de",
+};
+
+export const parmesan: IngredientType = {
+  name: "Parmesan",
+  type: "fromage",
+  unit: "g",
+  prefix: "de",
+};
+
+export const onion: IngredientType = {
+  name: "Oignon",
+  type: "vegetable",
+  unit: "unit",
+  prefix: "un",
+};
+
+export const garlic: IngredientType = {
+  name: "Ail",
+  type: "vegetable",
+  unit: "clove",
+  prefix: "une",
+};
+
+export const oliveOil: IngredientType = {
+  name: "Huile d'olive",
+  type: "huile",
+  unit: "ml",
+  prefix: "de",
+};
+
+export const salt: IngredientType = {
+  name: "Sel",
+  type: "épice",
+  unit: "tsp",
+  prefix: "de",
+};
+
+export const pepper: IngredientType = {
+  name: "Poivre",
+  type: "épice",
+  unit: "tsp",
+  prefix: "de",
+};
+
+export const parsley: IngredientType = {
+  name: "Persil",
+  type: "herbe",
+  unit: "g",
+  prefix: "de",
+};
