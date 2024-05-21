9782ef1dcf8fe5ea5286feb6c8ba5b0bf15a7ad6
keonwoo98 <keonwoo98@naver.com>
2024-05-21 13:02:18 +0200

    [feat] add props to TagList & update Tags props

--- src/Tagfilter/ListeDesTags.tsx
+++ src/Tagfilter/ListeDesTags.tsx
@@ -1,6 +1,12 @@
 import { Tags } from "../Tags/Tags";
 import { TagType } from "../Tags/TagType";
 
-export const TagList = ({ tags }: { tags: TagType[] }) => {
-  return <Tags tags={tags} />;
+interface TagListProps {
+  tags: TagType[];
+  selectedTags: string[];
+  setFilter: (tagId: string) => void;
+}
+
+export const TagList = ({ tags, selectedTags, setFilter }: TagListProps): JSX.Element => {
+  return <Tags tags={tags} selectedTags={selectedTags} setFilter={setFilter} />;
 };
