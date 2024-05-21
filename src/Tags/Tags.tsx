8d30299ea3c1baa7c6c3fc25ac9d4b33fc5ce297
keonwoo98 <keonwoo98@naver.com>
2024-05-21 13:06:38 +0200

    [feat] add props to Tags & update props to Tag

--- src/Tags/Tags.tsx
+++ src/Tags/Tags.tsx
@@ -1,7 +1,14 @@
 import { Tag } from "./Tag";
 import { TagType } from "./TagType";
 
-export const Tags = ({ tags }: { tags: TagType[] }) => {
+interface TagsProps {
+  tags: TagType[];
+  selectedTags: string[];
+  setFilter: (tagId: string) => void;
+  isClickable?: boolean;
+}
+
+export const Tags = ({ tags, selectedTags, setFilter, isClickable = true }: TagsProps): JSX.Element => {
   return (
     <div
       style={{
@@ -13,8 +20,8 @@ export const Tags = ({ tags }: { tags: TagType[] }) => {
         justifyContent: "center",
       }}
     >
-      {tags.map((obj) => (
-        <Tag tag={obj} />
+      {tags.map((tag) => (
+        <Tag key={tag.id} tag={tag} selectedTags={selectedTags} setFilter={setFilter} isClickable={isClickable} />
       ))}
     </div>
   );
